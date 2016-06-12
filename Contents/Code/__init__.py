046b5cf20184e0b77d06f7f1e03d3d620e6c3da2
David Chandler <davidjchandler@gmail.com>
2016-06-12 17:54:16 +0100

    fix: fix thumbnails and add basic error handling.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 TITLE = 'Pluralsight Courses'
 PREFIX = '/video/pluralsight'
 
@@ -5,6 +7,15 @@ ART = 'art-default.png'
 ICON = 'icon-default.png'
 
 g_client = SharedCodeService.client.Client()
+
+def handle_client_error(func):
+    @wraps(func)
+    def func_wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except SharedCodeService.client.ClientError as e:
+            return MessageContainer(header = L('ERROR_HEADER'), message = e.message)
+    return func_wrapper
 
 def Start():
     Log.Info('Starting %s plugin.', TITLE)
@@ -15,7 +26,7 @@ def Start():
     try:
         g_client.login()
         Log.Debug('Logged in.')
-    except LoginError as e:
+    except SharedCodeService.client.LoginError as e:
         Log.Error('ERROR: Login failed. %s', repr(e))
 
     ObjectContainer.title1 = TITLE
@@ -27,8 +38,8 @@ def ValidatePrefs():
     Log.Info('Preferences changed.')
     try:
         g_client.login(reset = True)
-    except LoginError as e:
-        return MessageContainer(header = L('LOGIN_FAILED_HEADER'), message = repr(e))
+    except SharedCodeService.client.LoginError as e:
+        return MessageContainer(header = L('LOGIN_FAILED_HEADER'), message = e.message)
     return MessageContainer(header = L('LOGIN_SUCCEEDED_HEADER'), message = L('LOGIN_SUCCEEDED_MESSAGE'))
 
 @handler(PREFIX, TITLE)
@@ -48,13 +59,14 @@ def MainMenu():
     return oc
 
 @route(PREFIX + '/recent')
+@handle_client_error
 def RecentMenu():
     Log.Info('RecentMenu')
     recentlyViewed = g_client.recently_viewed()
     oc = ObjectContainer(
-             title2 = L('RECENT_COURSES'),
-             no_cache = True
-         )
+            title2 = L('RECENT_COURSES'),
+            no_cache = True
+        )
 
     for recent in recentlyViewed:
         oc.add(
@@ -64,6 +76,7 @@ def RecentMenu():
     return oc
 
 @route(PREFIX + '/course')
+@handle_client_error
 def Course(courseName):
     Log.Info('Course: %s', courseName)
 
@@ -71,10 +84,11 @@ def Course(courseName):
 
     courseContainer = DirectoryObject(
         key = Callback(Modules, courseName = courseName),
-        title = course.title,
+        title = course.title + ' ({0})'.format(course.level),
         duration = course.duration,
         summary = course.description,
-        thumb = Resource.ContentsOfURLWithFallback(url = course.image)
+        thumb = course.image,
+        art = course.image
     )
 
     Log.Debug('Course \'%s\' has %d modules.', courseName, len(course.modules))
@@ -82,6 +96,7 @@ def Course(courseName):
     return courseContainer
 
 @route(PREFIX + '/modules')
+@handle_client_error
 def Modules(courseName):
     Log.Info('Modules: %s', courseName)
 
@@ -99,13 +114,15 @@ def Modules(courseName):
             title = module.title,
             duration = module.duration,
             summary = module.description,
-            thumb = Resource.ContentsOfURLWithFallback(url = course.image),
-            episode_count = len(module.clips)
+            thumb = course.image,
+            episode_count = len(module.clips),
+            tags = [course.level]
         ))
 
     return oc
 
 @route(PREFIX + '/clips')
+@handle_client_error
 def Clips(courseName, moduleName):
     Log.Info('Clips: %s - %s', courseName, moduleName)
 
@@ -126,7 +143,7 @@ def Clips(courseName, moduleName):
             show = module.title,
             duration = clip.duration,
             writers = [module.author],
-            thumb = Resource.ContentsOfURLWithFallback(course.image)
+            thumb = course.image
         ))
 
     return oc
