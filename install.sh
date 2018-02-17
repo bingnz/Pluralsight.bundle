554d5f45d3f8010ba5a840798de6ad846db1f985
Christian Sauer <sauerc@gmail.com>
2018-02-17 10:28:12 -0500

    Add output so you can tell something is happening

--- install.sh
+++ install.sh
@@ -3,17 +3,22 @@
 plexServer=$(launchctl list | grep -i plex | awk '{print $1}')
 
 if [ ! -z $plexServer ]; then
+	echo "Stopping server..."
     kill $plexServer
 fi
 
+echo "Removing existing..."
 rm -rf ~/Library/Application\ Support/Plex\ Media\ Server/Plug-in\ Support/Caches/com.plexapp.plugins.pluralsight
 rm -rf ~/Library/Application\ Support/Plex\ Media\ Server/Plug-in\ Support/Data/com.plexapp.plugins.pluralsight
 rm -rf ~/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/Pluralsight.bundle
 rm -f  ~/Library/Logs/Plex\ Media\ Server/PMS\ Plugin\ Logs/com.plexapp.plugins.pluralsight*
 
+echo "Installing latest..."
 cp -r . ~/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/Pluralsight.bundle
 
 if [ ! -z $plexServer ]; then
+	echo "Starting server..."
     sleep 2
     open -a /Applications/Plex\ Media\ Server.app
 fi
+echo "Done."
